9a4f37efed0731ca45d1657a4110cc8a67553eea
Linus Landin <linus.landin@edu.lycksele.se>
2021-03-17 09:11:45 +0100

    Add project files.

---
+++ TCPServer/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace TCPServer
+{
+    partial class TCPServer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.tbxInbox = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(587, 112);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(145, 47);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start Server";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // tbxInbox
+            // 
+            this.tbxInbox.Location = new System.Drawing.Point(211, 241);
+            this.tbxInbox.Name = "tbxInbox";
+            this.tbxInbox.Size = new System.Drawing.Size(386, 131);
+            this.tbxInbox.TabIndex = 1;
+            this.tbxInbox.Text = "";
+            this.tbxInbox.TextChanged += new System.EventHandler(this.tbxInbox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(208, 215);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Inbox";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // TCPServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxInbox);
+            this.Controls.Add(this.btnStart);
+            this.Name = "TCPServer";
+            this.Text = "Server";
+            this.Load += new System.EventHandler(this.TCPServer_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.RichTextBox tbxInbox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TCPServer/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace TCPServer
+{
+    public partial class TCPServer : Form
+    {
+        TcpListener listener;
+        TcpClient client;
+        int port = 12345;
+        
+        public TCPServer()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TCPServer_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+            client = listener.AcceptTcpClient();
+
+            byte[] inData = new byte[256];
+            int byteAmount = client.GetStream().Read(inData, 0, inData.Length);
+
+            tbxInbox.Text = Encoding.Unicode.GetString(inData, 0, byteAmount);
+            client.Close();
+            listener.Stop();
+        }
+
+        private void tbxInbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

12c4a18f47f65711e4b7d0ece3f1f1072223a288
Linus Landin <linus.landin@edu.lycksele.se>
2021-03-25 11:31:31 +0100

    Commented Server code

--- TCPServer/Form1.cs
+++ TCPServer/Form1.cs
@@ -34,16 +34,16 @@ namespace TCPServer
 
         }
 
-        private void btnStart_Click(object sender, EventArgs e)
+        private void btnStart_Click(object sender, EventArgs e) //När knappen trycks på så startas servern
         {
-            listener = new TcpListener(IPAddress.Any, port); 
-            listener.Start();
-            client = listener.AcceptTcpClient();
+            listener = new TcpListener(IPAddress.Any, port); //En ny lyssnare till IP adressen och porten skapas
+            listener.Start(); //Lyssnaren startas
+            client = listener.AcceptTcpClient(); //Lyssnaren accepterar förfrågan att ansluta från klienten.
 
             byte[] inData = new byte[256];
-            int byteAmount = client.GetStream().Read(inData, 0, inData.Length);
+            int byteAmount = client.GetStream().Read(inData, 0, inData.Length); //Servern läser in meddelandet skickat från klienten. 
 
-            tbxInbox.Text = Encoding.Unicode.GetString(inData, 0, byteAmount);
+            tbxInbox.Text = Encoding.Unicode.GetString(inData, 0, byteAmount); //Servern skriver ut meddelandet som kommer från klienten i textrutan.
             client.Close();
             listener.Stop();
         }
